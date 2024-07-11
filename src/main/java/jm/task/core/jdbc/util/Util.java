e6d52befe359954c6b9c28046fec477ef40776a4
Tagir Askarov <askarovtagir1@gmail.com>
2024-07-11 20:33:28 +0300

    made it

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+        private static final String URL = "jdbc:mysql://localhost:3306/lesson1";
+        private static final String USER = "root";
+        private static final String PASSWORD = "justfarm22";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL,USER,PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+
+
 }
